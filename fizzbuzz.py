ce9f22dc59787341e6bfb373187d77b9f5a567ca
Sumshi <sumayasomow@gmail.com>
2023-07-14 14:33:11 +0000

    leetcode fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        result = []
+        for i in range(1, n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                result.append("FizzBuzz")
+            elif i % 3 == 0:
+                result.append("Fizz")
+            elif i % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(i))
+        return result
+
+s1 = Solution()
+print(s1.fizzBuzz(3))   # Output: ["1", "2", "Fizz"]
+print(s1.fizzBuzz(5))   # Output: ["1", "2", "Fizz", "4", "Buzz"]
+print(s1.fizzBuzz(15))  # Output: ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]
