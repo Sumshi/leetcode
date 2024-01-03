3b28140d4322fa2d2d9cae07a1dbb9c270ec2ffb
Sumshi <sumayasomow@gmail.com>
2024-01-03 20:29:02 +0000

    palindrome

--- count_characters.py
+++ count_characters.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # count the number of times characters appear in a string
 def count_characters(s):
-    l = list(s)
+    l = list(s) # convert the string to a list
+    # count the number of times each character appears in the list
     frequency = [l.count(element) for element in l]
-    return dict(zip(l, frequency))
+    return dict(zip(l, frequency))# zip to combine the two lists into a dictionary
 
 # Example usage
 input_str = "hello"
